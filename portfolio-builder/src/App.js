ddffad003e34577c5bff04c1a7c092533ba3f824
Sam Thabet <sallam.thabet@gmail.com>
2021-11-07 17:42:52 -0500

    added props modified nav updated react to show changes when category name is chosen

--- portfolio-builder/src/App.js
+++ portfolio-builder/src/App.js
@@ -1,12 +1,29 @@
-import "./App.css";
+import React, { useState } from "react";
 import About from "./components/About/index";
 import Nav from "./components/Nav";
+import Gallery from "./components/Gallery";
 
 function App() {
+  const [categories] = useState([
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
   return (
     <div>
       <Nav />
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
