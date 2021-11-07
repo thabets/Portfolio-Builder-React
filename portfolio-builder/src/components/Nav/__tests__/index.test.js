33c3a256fa832e677b054da983db3a692b1efc6f
Sam Thabet <sallam.thabet@gmail.com>
2021-11-07 14:19:55 -0500

    added testing features to profile

---
+++ portfolio-builder/src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Nav from "..";
+
+afterEach(cleanup);
+
+describe("Nav component", () => {
+  //baseline test
+  it("renders", () => {
+    render(<Nav />);
+  });
+  //snapshot test
+  it("matches snapshot DOM node structure", () => {
+    const { asFragment } = render(<Nav />);
+    //value comparison
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+describe("emoji is visible", () => {
+  it("inserts emoji into the h2", () => {
+    //Arrange
+    const { getByLabelText } = render(<Nav />);
+    //Assert
+    expect(getByLabelText("camera")).toHaveTextContent("📸");
+  });
+});
+
+describe("links are visible", () => {
+  it("inserts text into links", () => {
+    //Arrange
+    const { getByTestId } = render(<Nav />);
+    //Assert
+    expect(getByTestId("link")).toHaveTextContent("Oh Snap!");
+    expect(getByTestId("about")).toHaveTextContent("About me");
+  });
+});
